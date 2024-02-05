05e41f377deb20c45be69ad66c756dc278414aa4
Korrd <lordkorrd@gmail.com>
2024-02-05 12:54:57 +0100

    Support custom tracks output path

--- config.py
+++ config.py
@@ -8,37 +8,37 @@ class config:
         return config['access_token'], \
               config['refresh_token'], \
               config['client_id'], \
-              config['client_secret'], \
-              ""
+              config['client_secret']
       else:
         return config['strava_access_token'], \
               config['strava_refresh_token'], \
               config['strava_client_id'], \
-              config['strava_client_secret'], \
-              config['dropbox_access_token']
+              config['strava_client_secret']
 
   def write_secrets_file(secrets_file: str,
                         strava_client_id: str = "",
                         strava_client_secret: str = "",
                         strava_access_token: str = "",
-                        strava_refresh_token: str = "",
-                        dropbox_access_token: str = ""):
+                        strava_refresh_token: str = ""):
     with open(f"{secrets_file}", mode="w") as f:
       config = {}
       config['strava_access_token'] = strava_access_token
       config['strava_refresh_token'] = strava_refresh_token
       config['strava_client_id'] = strava_client_id
       config['strava_client_secret'] = strava_client_secret
-      config['dropbox_access_token'] = dropbox_access_token
       f.write(json.dumps(config))
 
   def read_config_file(config_file: str) -> str:
     with open(f"{config_file}", mode="r") as f:
       config = json.loads(f.read())
-      return config['dropbox_path']
+      return config['tracks_output_path']
 
-  def write_config_file(config_file: str, dropbox_path: str):
+  def write_config_file(config_file: str, tracks_output_path: str):
     config = {}
-    config['dropbox_path'] = dropbox_path
+    config['tracks_output_path'] = tracks_output_path
     with open(f"{config_file}", mode="w") as f:
       f.write(json.dumps(config))
+
+  def ask_for_tracks_output_path() -> str:
+    print("\033[93m⚠️  [Optional] Please, provide a full path to a folder to store your tracks on")
+    return input("\033[95m📂 Output Folder: \033[0m")
